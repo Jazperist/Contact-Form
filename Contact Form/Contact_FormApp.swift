e2206ed389fe549b955039d83ce1a71767624c2e
Jazperist <39943709+Jazperist@users.noreply.github.com>
2023-05-23 23:48:52 -0600

    Add files via upload

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Rodrigo Garcia Olvera on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
